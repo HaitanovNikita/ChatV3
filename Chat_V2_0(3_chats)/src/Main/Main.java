90f3c58ab000f60902a0ee9482dc911b44f5914f
HaitanovNikita <52675423+HaitanovNikita@users.noreply.github.com>
2019-07-11 12:47:11 +0300

    Update project(add static context)
    Add to project: chat.html,login.html, add to class Server: static class StaticHandler

---
+++ Chat_V2_0(3_chats)/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Server.Server;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Server server = new Server();
+            server.starting();
+
+
+    }
+}
